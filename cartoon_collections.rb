fcbc36faa74003a33b52070d9b5fc76831a0543b
David McGrann <dpmcgrann@gmail.com>
2018-03-10 18:09:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name)
+  name.each_with_index { |dwarf| puts "#{i} #{dwarf}" }
 end
 
 def summon_captain_planet# code an argument here
