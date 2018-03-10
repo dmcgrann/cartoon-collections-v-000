8d420c94b7836ec80bef9d0fcda7333136885d33
David McGrann <dpmcgrann@gmail.com>
2018-03-10 18:17:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,8 @@ end
 
 def summon_captain_planet(array)
   array1 = []
-  array.collect { |call| puts array1 << "#{call!}" }
+  array.collect { |call| puts array1 << call + "!" }
+  array1
 end
 
 def long_planeteer_calls# code an argument here
