9f8c7a3dad59759e9d9e14a81187691c1a63e061
David McGrann <dpmcgrann@gmail.com>
2018-03-10 19:38:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(name)
 end
 
 def summon_captain_planet(a)
-  a.split.map {|w| w.upcase + "!" }
+  a.map {|w| w.to_s.upcase + "!" }.join(", ")
 end
 
 def long_planeteer_calls# code an argument here
