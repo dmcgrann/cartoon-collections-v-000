8b53c183fe9b1d03d346be8c6d28e1efd53fbe95
David McGrann <dpmcgrann@gmail.com>
2018-03-10 21:01:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array = []
-  array.detect { |x| cheese_types.include?(x) }
+  array.detect{|x| cheese_types.include?(x)}
 end
